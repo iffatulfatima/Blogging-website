0e620c550497d1bb95527d970ce6c32c27c05a36
iffatulfatima <157104458+iffatulfatima@users.noreply.github.com>
2024-10-15 20:49:29 +0500

    Add files via upload

---
+++ src/contact/page.tsx
@@ -0,0 +1,15 @@
+
+import ContactSection from "../app/compoents/Contact";
+
+
+
+export default function About() {
+    return (
+        <>
+      <div className='contact'>
+        <h1>Contact</h1>
+        <ContactSection />
+      </div>
+        </>
+    );
+}
